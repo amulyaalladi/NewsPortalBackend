9aa930e585818a2091991f24f2f59af63ae50830
amulyaalladi <amulyaalladi@gmail.com>
2026-08-19 11:00:22 +0530

    updated profile settings

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { me } = require('../controllers/authController');
+const userController = require('../controllers/userController');
 const { isAuthenticated } = require('../middleware/auth');
 
 const userRouter = express.Router();
 
 userRouter.get('/me', isAuthenticated, me);
+userRouter.put('/me', isAuthenticated, userController.updateProfile);
 
 module.exports = userRouter;
